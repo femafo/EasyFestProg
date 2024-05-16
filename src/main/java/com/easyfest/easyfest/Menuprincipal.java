3d77e93c0fd7922a0a509e47b5cdd68b8cb3a033
Daniela <davaga01@xuqueralumnat.es>
2024-05-16 11:43:28 +0200

    cambios en menu principal

--- src/main/java/com/easyfest/easyfest/Menuprincipal.java
+++ src/main/java/com/easyfest/easyfest/Menuprincipal.java
@@ -1,7 +1,27 @@
 package com.easyfest.easyfest;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
 public class Menuprincipal
 {
+    @javafx.fxml.FXML
+    private Button mensajesId;
+    @javafx.fxml.FXML
+    private Button ayudaId;
+    @javafx.fxml.FXML
+    private Button ajustesId;
+    @javafx.fxml.FXML
+    private ChoiceBox chbUsuario;
+    @javafx.fxml.FXML
+    private Button busquedaId;
+    @javafx.fxml.FXML
+    private Button inicioId;
+    @javafx.fxml.FXML
+    private Button pagosId;
+    @javafx.fxml.FXML
+    private Button sobreNosotrosId;
+
     @javafx.fxml.FXML
     public void initialize() {
     }}

ef6b0f4addb54eaaa03f368f42772bfd78aa11c5
femafo <femafo@xuqueralumnat.es>
2024-05-16 13:42:18 +0200

    menu principal apañadp

--- src/main/java/com/easyfest/easyfest/PagosController.java
+++ src/main/java/com/easyfest/easyfest/PagosController.java
@@ -1,4 +1,22 @@
 package com.easyfest.easyfest;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class PagosController {
+    @javafx.fxml.FXML
+    private AnchorPane pagosid;
+    @javafx.fxml.FXML
+    private Button addtarjetaid;
+    @javafx.fxml.FXML
+    private Button vermasid;
+
+    @javafx.fxml.FXML
+    public void vermasbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void anadirbutton(ActionEvent actionEvent) {
+    }
 }

1ab3fb4021715e3dac1671e662a3b083e2c2aa85
ivan <ivhoal@xuqueralumnat.es>
2024-05-17 12:00:52 +0200

    Cositas

--- src/main/java/com/easyfest/easyfest/HelloApplication.java
+++ src/main/java/com/easyfest/easyfest/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 1530, 780);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();

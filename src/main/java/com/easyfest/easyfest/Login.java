80c43401cb491b80e90c7f4de845fd80d46d97dc
Daniela <davaga01@xuqueralumnat.es>
2024-05-21 11:03:24 +0200

    inicio de sesion hecho

--- src/main/java/com/easyfest/easyfest/Login.java
+++ src/main/java/com/easyfest/easyfest/Login.java
@@ -9,6 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -40,6 +42,8 @@ public class Login implements Initializable {
     @FXML
     private AnchorPane LoginPageid;
     UsuariosModel um = new UsuariosModel();
+    @FXML
+    private Pane paneLogIn;
 
     /**
      * Método para manejar el evento de inicio de sesión.
@@ -107,5 +111,6 @@ public class Login implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Este método se llama automáticamente después de que se ha cargado el archivo FXML
         // Aquí puedes realizar inicializaciones adicionales si es necesario
+
     }
 }

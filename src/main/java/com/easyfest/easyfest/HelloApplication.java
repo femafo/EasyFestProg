9ab9f2f6db35c6e9a4cdd12930b39f57e18b3e38
femafo <femafo@xuqueralumnat.es>
2024-05-29 23:13:29 +0200

    menu admin realizado

--- src/main/java/com/easyfest/easyfest/HelloApplication.java
+++ src/main/java/com/easyfest/easyfest/HelloApplication.java
@@ -3,6 +3,7 @@ package com.easyfest.easyfest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,22 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1530, 780);
+        Image image = null;
+        String imgPath = "/img/logoEasyFest (1).png";
+        try {
+            image = new Image(getClass().getResourceAsStream(imgPath));
+            if (image.isError()) {
+                throw new Exception("Error loading image: " + image.getException());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Image not found or failed to load: " + imgPath);
+        }
 
+        if (image != null) {
+            stage.getIcons().add(image);
+        }
+        stage.getIcons().add(image);
         stage.setTitle("EasyFest");
         stage.setScene(scene);
         stage.show();

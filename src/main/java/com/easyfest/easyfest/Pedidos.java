8b93c6a045b3843ca8aa4533e79d5fac92f4f78a
ivan <ivhoal@xuqueralumnat.es>
2024-05-30 10:31:46 +0200

    Cositas

--- src/main/java/com/easyfest/easyfest/Pedidos.java
+++ src/main/java/com/easyfest/easyfest/Pedidos.java
@@ -1,12 +1,19 @@
+/**
+ * Clase controladora para la gestión de pedidos.
+ */
 package com.easyfest.easyfest;
 
 import javafx.scene.layout.AnchorPane;
 
-public class Pedidos
-{
+public class Pedidos {
+    // El identificador del panel de pedidos en el archivo FXML
     @javafx.fxml.FXML
     private AnchorPane pedidosid;
 
+    /**
+     * Método de inicialización llamado automáticamente al cargar el archivo FXML.
+     */
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+}

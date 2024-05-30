eee36b9dbf04fdc87f80908dd789a4dc9e143dc2
ivan <ivhoal@xuqueralumnat.es>
2024-05-30 11:56:31 +0200

    Cositas

--- src/main/java/com/easyfest/easyfest/MiListaC.java
+++ src/main/java/com/easyfest/easyfest/MiListaC.java
@@ -1,6 +1,15 @@
 package com.easyfest.easyfest;
-
+/**
+ * Interfaz que define un método para manejar el clic en un elemento de la lista de compras.
+ *
+ * @author femafo
+ */
 public interface MiListaC {
+    /**
+     * Método que se llama cuando se hace clic en un elemento de la lista de compras.
+     *
+     * @param productos el producto seleccionado
+     */
     public void onClickLista(Productos productos);
 
 

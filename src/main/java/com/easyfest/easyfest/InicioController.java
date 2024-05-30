e5e538cc4cf74d1e5cf415c7a8287f3970c8a1be
ivan <ivhoal@xuqueralumnat.es>
2024-05-30 12:25:08 +0200

    Cositas

--- src/main/java/com/easyfest/easyfest/InicioController.java
+++ src/main/java/com/easyfest/easyfest/InicioController.java
@@ -1,8 +1,15 @@
 package com.easyfest.easyfest;
 
 import javafx.scene.layout.AnchorPane;
-
+/**
+ * Controlador para la página de inicio.
+ *
+ * @author fermin
+ */
 public class InicioController {
+    /**
+     * AnchorPane para la página de inicio.
+     */
     @javafx.fxml.FXML
     private AnchorPane iniciopaneid;
 }

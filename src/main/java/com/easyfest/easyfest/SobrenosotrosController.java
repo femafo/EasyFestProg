d23ca41b1a1f6e2482eb5d7ccd36e40f875e0d02
ivan <ivhoal@xuqueralumnat.es>
2024-05-30 09:01:18 +0200

    Cositas

--- src/main/java/com/easyfest/easyfest/SobrenosotrosController.java
+++ src/main/java/com/easyfest/easyfest/SobrenosotrosController.java
@@ -2,9 +2,10 @@ package com.easyfest.easyfest;
 
 import javafx.scene.layout.AnchorPane;
 
+/**
+ * El controlador SobrenosotrosController controla la vista correspondiente a la sección "Sobre nosotros".
+ */
 public class SobrenosotrosController {
     @javafx.fxml.FXML
     private AnchorPane sobrenosotrosid;
-
-
 }
